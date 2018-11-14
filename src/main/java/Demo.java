ac0b5ab1093c860c1cc25d394c5e7a0af2939e63
chase <chase@mail>
2018-11-14 20:11:52 +0800

    Initial commit

---
+++ src/main/java/Demo.java
@@ -0,0 +1,11 @@
+/**
+ * @Auther chase
+ * @Time 2018/11/14 20:05
+ * @Version 1.0
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("明天星期四");
+    }
+}
